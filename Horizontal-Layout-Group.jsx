be2c6e17cf52fea30dec1dab4ac5e79e224b93cd
ANDYLAUBABY <andywinghang@gmail.com>
2022-06-04 21:38:17 +0800

    add animation

--- Horizontal-Layout-Group.jsx
+++ Horizontal-Layout-Group.jsx
@@ -1,23 +1,26 @@
-if(thisLayer.position[1]==( thisComp.height/2) ){
-	x=0;
-	g=0;
-	c=0;
+if(Math.abs(thisLayer.position[1]-thisComp.height/2) < (thisLayer.height/2+thisComp.height/2)){
+	x=0;//init for x position
+	g=0;//layer count
+	c=0;//index inside layer
+	f=0;//moveable layer count
+	//may be we can use array to store which index going down
 	for(var i=1;i<thisComp.numLayers+1;i++){
-		if(thisComp.layer(i).position[1] == thisComp.height/2)
+		if(Math.abs(thisComp.layer(i).position[1]-thisComp.height/2) < (thisLayer.height/2+thisComp.height/2))
 		{
 			g++;
 			if(thisComp.layer(i).transform == thisLayer.transform){
 				c=g;
 			}
+			f += 1-(Math.abs(thisComp.layer(i).position[1]-thisComp.height/2) /(thisLayer.height/2+thisComp.height/2));
 		}
 	}
-
-	if((thisComp.width/thisLayer.width) >g )
+	if((thisComp.width/thisLayer.width) > g)
 	{
 		x=c* thisLayer.width- thisLayer.width/2;
 	}else{
-		a = ((thisComp.width-thisLayer.width)/(g-1));
-		x=a*(c-1)+ thisLayer.width/2;
+		a = ((thisComp.width-thisLayer.width ))/(f-1);
+		// change 1 to f but change it after the card move
+		x = a*(c-1)+ thisLayer.width/2;
 	}
 	[x,value[1]]
 }
