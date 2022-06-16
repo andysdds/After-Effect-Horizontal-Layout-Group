13aecec1e474d58163087f3792e5a7788ce8ec86
ANDYLAUBABY <andywinghang@gmail.com>
2022-06-16 22:40:29 +0800

    change mind concept

--- Horizontal-Layout-Group.jsx
+++ Horizontal-Layout-Group.jsx
@@ -1,27 +1,34 @@
 if(Math.abs(thisLayer.position[1]-thisComp.height/2) < (thisLayer.height/2+thisComp.height/2)){
-	x=0;//init for x position
-	g=0;//layer count
-	c=0;//index inside layer
-	f=0;//moveable layer count
+	stack=0;
 	//may be we can use array to store which index going down
 	for(var i=1;i<thisComp.numLayers+1;i++){
-		if(Math.abs(thisComp.layer(i).position[1]-thisComp.height/2) < (thisLayer.height/2+thisComp.height/2))
+		if(thisComp.width/thisComp.numLayers > thisComp.layer(i).width)
 		{
-			g++;
-			if(thisComp.layer(i).transform == thisLayer.transform){
-				c=g;
+			if(i==1)
+			{
+				stack+=thisComp.layer(i).width/2;
 			}
-			f += 1-(Math.abs(thisComp.layer(i).position[1]-thisComp.height/2) /(thisLayer.height/2+thisComp.height/2));
+			else
+			{
+				stack+=thisComp.layer(i).width;
+			}
+		}
+		else{
+			if(i==1)
+			{
+				stack+=thisComp.layer(i).width/2;
+			}else{
+				//TODO 1 should be change
+				cardDistance = ((thisComp.width-thisLayer.width)/(thisComp.numLayers-1));
+				verticalNormal =Math.abs(thisComp.layer(i-1).position[1]-thisComp.height/2) /(thisLayer.height/2+thisComp.height/2);
+				stack+=cardDistance - (verticalNormal *cardDistance);
+			}
+		}
+		if(thisComp.layer(i).transform == thisLayer.transform){
+			break;
 		}
 	}
-	if((thisComp.width/thisLayer.width) > g)
-	{
-		x=c* thisLayer.width- thisLayer.width/2;
-	}else{
-		a = ((thisComp.width-thisLayer.width ))/(f-1);
-		// change 1 to f but change it after the card move
-		x = a*(c-1)+ thisLayer.width/2;
-	}
-	[x,value[1]]
+	
+	[stack,value[1]]
 }
 else value
