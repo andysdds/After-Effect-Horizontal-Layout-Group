0a1d24c891668f94ea3f9789a03221c28a44a181
ANDYLAUBABY <andywinghang@gmail.com>
2022-06-18 13:21:08 +0800

    fix count issue

--- Horizontal-Layout-Group.jsx
+++ Horizontal-Layout-Group.jsx
@@ -2,42 +2,38 @@ if (Math.abs(thisLayer.position[1] - thisComp.height / 2) < (thisLayer.height / 
 	stack = 0;
 	floatingCount = 0;
 	count = 0;
-	previous = 0;
-	previousFlag = 0;
+	previousHeight = 0;
 	cardDistance = 0;
 	verticalNormal = 0;
 	for (var i = 1; i < thisComp.numLayers + 1; i++) {
-		if (thisComp.layer(i).transform == thisLayer.transform) {
-			previous = previousFlag;
-		}
 		if (Math.abs(thisComp.layer(i).position[1] - thisComp.height / 2) <= (thisLayer.height / 2 + thisComp.height / 2)) {
 			floatingCount += 1 - Math.abs(thisComp.layer(i).position[1] - thisComp.height / 2) / (thisLayer.height / 2 + thisComp.height / 2);
 			count++;
-			previousFlag = i;
 		}
-
 	}
-	for (var i = 1; i < count + 1; i++) {
-		if (thisComp.width / count >= thisLayer.width) {
-			if (i == 1) {
-				stack += thisLayer.width / 2;
+	for (var i = 1; i < thisComp.numLayers + 1 + 1; i++) {
+		if (Math.abs(thisComp.layer(i).position[1] - thisComp.height / 2) <= (thisLayer.height / 2 + thisComp.height / 2)) {
+			if (thisComp.width / count >= thisLayer.width) {
+				if (i == 1) {
+					stack += thisLayer.width / 2;
+				}
+				else {
+					stack += thisLayer.width;
+				}
 			}
 			else {
-				stack += thisLayer.width;
+				if (i == 1) {
+					stack += thisComp.layer(i).width / 2;
+				} else {
+					cardDistance = ((thisComp.width - thisLayer.width) / (floatingCount - 1));
+					verticalNormal = Math.abs(previousHeight - thisComp.height / 2) / (thisLayer.height / 2 + thisComp.height / 2);
+					stack += cardDistance - (verticalNormal * cardDistance);
+				}
 			}
-		}
-		else {
-			if (i == 1) {
-				stack += thisComp.layer(i).width / 2;
-			} else {
-				cardDistance = ((thisComp.width - thisLayer.width) / (floatingCount - 1));
-				verticalNormal = Math.abs(thisComp.layer(previous).position[1] - thisComp.height / 2) / (thisLayer.height / 2 + thisComp.height / 2);
-				// this card don't know previous card x position
-				stack += cardDistance - (verticalNormal * cardDistance);
+			if (thisComp.layer(i).transform == thisLayer.transform) {
+				break;
 			}
-		}
-		if (thisComp.layer(i).transform == thisLayer.transform) {
-			break;
+			previousHeight = thisComp.layer(i).position[1];
 		}
 	}
 
