822359dc1c855db7fce570dc1a45fef897005bed
ANDYLAUBABY <andywinghang@gmail.com>
2022-06-18 14:56:05 +0800

    fix full to 1 issue

--- Horizontal-Layout-Group.jsx
+++ Horizontal-Layout-Group.jsx
@@ -13,22 +13,16 @@ if (Math.abs(thisLayer.position[1] - thisComp.height / 2) < (thisLayer.height / 
 	}
 	for (var i = 1; i < thisComp.numLayers + 1 + 1; i++) {
 		if (Math.abs(thisComp.layer(i).position[1] - thisComp.height / 2) <= (thisLayer.height / 2 + thisComp.height / 2)) {
-			if (thisComp.width / count >= thisLayer.width) {
-				if (i == 1) {
-					stack += thisLayer.width / 2;
-				}
-				else {
-					stack += thisLayer.width;
-				}
-			}
-			else {
-				if (i == 1) {
-					stack += thisComp.layer(i).width / 2;
+			if (i == 1) {
+				stack += thisComp.layer(i).width / 2;
+			} else {
+				if (((thisComp.width - thisLayer.width) / (floatingCount - 1)) >= thisLayer.width) {
+					cardDistance = thisLayer.width;
 				} else {
 					cardDistance = ((thisComp.width - thisLayer.width) / (floatingCount - 1));
-					verticalNormal = Math.abs(previousHeight - thisComp.height / 2) / (thisLayer.height / 2 + thisComp.height / 2);
-					stack += cardDistance - (verticalNormal * cardDistance);
 				}
+				verticalNormal = Math.abs(previousHeight - thisComp.height / 2) / (thisLayer.height / 2 + thisComp.height / 2);
+				stack += cardDistance - (verticalNormal * cardDistance);
 			}
 			if (thisComp.layer(i).transform == thisLayer.transform) {
 				break;
@@ -36,7 +30,6 @@ if (Math.abs(thisLayer.position[1] - thisComp.height / 2) < (thisLayer.height / 
 			previousHeight = thisComp.layer(i).position[1];
 		}
 	}
-
 	[stack, value[1]]
 }
 else value
